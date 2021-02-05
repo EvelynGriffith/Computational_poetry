8c8350862b4c19833b39c9dabd1aff57e018308c
EvelynGriffith <griffith01@allegheny.edu>
2021-02-05 10:41:09 -0500

    saving progress

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-
-import random
-from textwrap3 import wrap
-import markovify
-import string
     
 ####################################
+import random
+import string
+
+def read_file(filename):
+    book = open(f"data/{filename}","r")
+    return book.read()
+
 def scramble(word):
     word = "".join([ch for ch in word if ch not in string.punctuation])
     if word not in keeps:
@@ -13,30 +15,31 @@ def scramble(word):
         return "".join(scrambled)
     return word
 
-def read_file(file):
-    book = open(f"data/{file}","r")
-    return book.read()
-
-
-keeps = ["the"]
-text = [
+keeps = ["the", "it", "for", "and", "to", "are"]
+texts = [
     "aesop.txt",
     "fairytales.txt",
     "prideandprejudice.txt"
 ]
-texts = ""
 
-for book in text:
-    texts += read_file(book) + "\n"
-lines = texts.split("\n")
-words = texts.split(" ")
+text = ""
 
+lines = text.split("\n")
 
+lines = [line for line in lines if line != ""] # <---"list comprehension"
+    # <-- if i print(lines) it will print it in a huge chuck cause of previous command
+    
+for book in texts:
+    text += read_file(book) + "\n"
+    
 for line in text:
     words = line.split(" ")
     for word in words:
         new_word = scramble(word)
         line = line.replace(word,new_word)
-    print(line)
-    
 
+
+poem = "\n".join(random.sample(line,20))
+
+with open("Jabberwocky2.0", "w") as output:
+    output.write(poem)
