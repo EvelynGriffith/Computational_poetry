c334dd453eb4bfbdbfacb448abc2dd3b5456f6c6
EvelynGriffith <griffith01@allegheny.edu>
2021-02-06 09:27:31 -0500

    wrote progress report

--- src/main.py
+++ src/main.py
@@ -3,61 +3,44 @@
 ####################################
 import random
 import string
-import markovify
+import markovify # <-- importing markovifym random, string and textwrap3
+from textwrap3 import wrap
 
 def read_file(filename):
-    book = open(f"data/{filename}","r")
+    book = open(f"data/{filename}","r")#<-- getting the program to read through the three files in data folder
     return book.read()
 
 def scramble(word):
-    word = "".join([ch for ch in word if ch not in string.punctuation])
-    if word not in keeps:
-        scrambled = random.sample(word,len(word))
-        return "".join(scrambled)
+    word = "".join([ch for ch in word if ch not in string.punctuation])#<-- this should delete punctuation
+    if word not in keeps:#<-- see keeps list down below to see which words will not be scrambled
+        scrambled = random.sample(word,len(word))#<-- scrambling words
+        return "".join(scrambled)#<-- joining scrambled words
     return word
 
-keeps = ["the", "it", "for", "and", "to", "are"]
+keeps = ["the", "it", "for", "and", "to", "are"] #<-- keeping certain words from being scrambled
 texts = [
     "aesop.txt",
     "fairytales.txt",
-    "prideandprejudice.txt"
+    "prideandprejudice.txt"#<-- the three books I downloaded to take words from
 ]
 
-text = ""
+text = ""#<-- defining text for later commands
 
-scrambled = ""
+scrambled = ""#<-- defining scrambled for later commands
 
-for book in texts:
+for book in texts:#<-- getting the program to read the specific books
     text += read_file(book) + "\n"
 corpus = text.split(" ")
 corpus = set(corpus)
     
-for word in corpus:
-#     words = line.split(" ")
-#     for word in words:
+for word in corpus:#<-- scrambling the words and replacing words with new scrambled words
     new_word = scramble(word)
     text = text.replace(word,new_word)
-   # poem = "\n".join(random.sample(line,20))
-    #print(line)
-        
-with open("Jabberwocky2.0", "w") as output:
-    output.write(text)
-
-    
-model = markovify.NewlineText(text,state_size=1)
-
-# Generate 10 sentences
-for _ in range(10):
-    scrambled += model.make_short_sentence((60) + "\n") 
-print(scrambled)   
-# with open("Jabberwocky2.0", "w") as output:
-#     output.write(text)
+    wrapped = wrap(" ".join(text), 40)#<-- attempting to join text and wrap it in poem format
 
 
-# lines = text.split("\n")
-
-# lines = [line for line in lines if line != ""] # <---"list comprehension"
-#     # <-- if i print(lines) it will print it in a huge chuck cause of previous command
+with open("Jabberwocky2.0", "w") as output:#<-- printing new poem as 'Jabberwocky2.0' and writing it in a seperate file
+     output.write(text)
      
     
     
